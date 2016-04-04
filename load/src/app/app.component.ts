27d9245ae5714ee305c34a4231aeabcc5628ff85
kartzum <kartzum@gmail.com>
2016-04-04 23:38:43 +0300

    load. d3.

--- load/src/app/app.component.ts
+++ load/src/app/app.component.ts
@@ -1,3 +1,5 @@
+/// <reference path="../typings/d3/d3.d.ts"/>
+
 import {Component, OnInit} from 'angular2/core';
 import {DataService} from './data.service'
 
@@ -5,21 +7,51 @@ import {DataService} from './data.service'
     selector: 'app',
     template: `
     <h1>App</h1>
-    <ul>
-      <li *ngFor="#d of data">
-        {{ d }}
-      </li>
-    </ul>
     `,
     providers:[DataService]
 })
 export class AppComponent implements OnInit {
-    private data:any;
 
     constructor(private _dataService:DataService) {
     }
 
     ngOnInit(): any {
-        this._dataService.load().then(value => this.data = value);
+        this._dataService.load().then(value => {
+            var force = d3.layout.force()
+                .charge(-120)
+                .linkDistance(30)
+                .size([500, 500]);
+
+            var svg = d3.select("body").append("svg")
+                .attr("width", 500)
+                .attr("height", 500);
+
+            force
+                .nodes(value.nodes)
+                .links(value.links)
+                .start();
+
+            var node = svg.selectAll(".node")
+                .data(value.nodes)
+                .enter().append("circle")
+                .attr("class", "node")
+                .attr("r", 5);
+
+            var link = svg.selectAll(".link")
+                .data(value.links)
+                .enter().append("line")
+                .attr("class", "link")
+                .style("stroke-width", function(d) { return Math.sqrt(d.value); });
+
+            force.on("tick", function() {
+                link.attr("x1", function(d) { return d.source.x; })
+                    .attr("y1", function(d) { return d.source.y; })
+                    .attr("x2", function(d) { return d.target.x; })
+                    .attr("y2", function(d) { return d.target.y; });
+
+                node.attr("cx", function(d) { return d.x; })
+                    .attr("cy", function(d) { return d.y; });
+            });
+        });
     }
 }
--- load/src/app/data.service.ts
+++ load/src/app/data.service.ts
@@ -3,8 +3,15 @@ import {Injectable} from 'angular2/core'
 @Injectable()
 export class DataService {
   public load():Promise<any> {
-      var data = [];
-      data.push({data: 1});
+      var data = {
+          "nodes":[
+              {"name":"Myriel","group":1},
+              {"name":"Napoleon","group":1},
+          ],
+          "links":[
+              {"source":1,"target":0,"value":1}
+          ]
+      };
       return Promise.resolve(data);
   }
 }
