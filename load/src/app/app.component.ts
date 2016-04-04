1576dbb1f1e95162445580f0a41ccfc4f90b385b
kartzum <kartzum@gmail.com>
2016-04-04 22:31:39 +0300

    load. Initial.

---
+++ load/src/app/app.component.ts
@@ -0,0 +1,11 @@
+/// <reference path="../node_modules/angular2/core" />
+
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'app',
+    template: `
+    <h1>App</h1>
+    `
+})
+export class AppComponent { }
---
+++ load/src/app/boot.ts
@@ -0,0 +1,8 @@
+/// <reference path="../node_modules/angular2/platform/browser" />
+
+import {bootstrap} from 'angular2/platform/browser'
+
+import {AppComponent} from './app.component'
+
+bootstrap(AppComponent, [
+]);
